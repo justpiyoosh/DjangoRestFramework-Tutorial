5c48f8226a7ea627b852c3ec676815451f0649ac
justpiyoosh <justpiyoosh@gmail.com>
2020-11-06 00:57:40 +0530

    Create modes and serializer for Casion

--- finder/models.py
+++ finder/models.py
@@ -7,3 +7,6 @@ class Casino(models.Model):
     address = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
---
+++ finder/serializers.py
@@ -0,0 +1,7 @@
+from finder.models import Casino
+from rest_framework import serializers
+
+class CasinoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Casino
+        field = ('id' , 'name' , 'address')
